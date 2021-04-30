cf9600ec7029736eb52599f1fff38c2246a147cc
brcusri <burcu.iras@gmail.com>
2021-04-30 07:01:53 +0300

    güncelleme-1

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from books.models import Category, Book
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status']
+    list_filter = ['status']
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['title', 'status','amount', 'edition', 'year', 'author']
+    list_filter = ['status', 'category']
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Book,BookAdmin)
+
+
